242be8f59e24bb5a4f75e09181fca92a3f5c012f
DemoRunner <DemoRunner@163.com>
2015-10-20 20:02:11 +0800

    softmax
    softmax exercise can not solve,you should think thoughtly.

---
+++ softmax_exercise/computeNumericalGradient.m
@@ -0,0 +1,36 @@
+function numgrad = computeNumericalGradient(J, theta)
+% numgrad = computeNumericalGradient(J, theta)
+% theta: a vector of parameters
+% J: a function that outputs a real-number. Calling y = J(theta) will return the
+% function value at theta. 
+  
+% Initialize numgrad with zeros
+numgrad = zeros(size(theta));
+
+% ---------- YOUR CODE HERE --------------------------------------
+% Instructions: 
+% Implement numerical gradient checking, and return the result in numgrad.  
+% (See Section 2.3 of the lecture notes.)
+% You should write code so that numgrad(i) is (the numerical approximation to) the 
+% partial derivative of J with respect to the i-th input argument, evaluated at theta.  
+% I.e., numgrad(i) should be the (approximately) the partial derivative of J with 
+% respect to theta(i).
+%                
+% Hint: You will probably want to compute the elements of numgrad one at a time. 
+epsilon = 1e-4;
+for i = 1:size(theta)  
+    theta_add = theta;  
+    theta_sub = theta;  
+    theta_add(i) = theta_add(i) + epsilon;  
+    theta_sub(i) = theta_sub(i) - epsilon;  
+    numgrad(i) = (J(theta_add) - J(theta_sub)) / (2 * epsilon);  
+end  
+
+
+
+
+
+
+
+% ---------------------------------------------------------------
+end
