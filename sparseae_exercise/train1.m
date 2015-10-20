ccf8fa785141ab008c9152274f8876da31744a9c
DemoRunner <DemoRunner@163.com>
2015-10-20 22:59:45 +0800

    non
    non

--- sparseae_exercise/train1.m
+++ sparseae_exercise/train1.m
@@ -35,7 +35,7 @@ labels = loadMNISTLabels('train-labels.idx1-ubyte');
  
 %We are using display_network from the autoencoder code
 display_network(images(:,1:100)); % Show the first 100 images
-disp(labels(1:10));
+%disp(labels(1:10));
 
 
 patches = images(:,1:1000);
